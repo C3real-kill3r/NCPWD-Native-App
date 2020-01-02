1ae9c222cecc5b541f3c98b8a5442ac618760d92
C3real-kill3r <brybzi@gmail.com>
2020-01-02 19:58:50 +0300

    add authentication actions

---
+++ NCPWD/src/actions/auth.action.js
@@ -0,0 +1,21 @@
+import {fetchApi} from '../service/api';
+
+export const createNewUser = payload => {
+  return async dispatch => {
+    try {
+      const response = await fetchApi('users/', 'POST', payload, 200);
+    } catch (error) {
+      throw error;
+    }
+  };
+};
+
+export const loginUser = payload => {
+  return async dispatch => {
+    try {
+      const response = await fetchApi('users/login/', 'POST', payload, 200);
+    } catch (error) {
+      throw error;
+    }
+  };
+}
